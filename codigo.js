258b58e9d8193aca852c168f333bb6debf830db3
Brandon Miguel Castillo Llajamango <pibelanzallamas@MacBook-Pro-de-Brandon.local>
2023-11-11 08:32:39 -0300

    mobile responsive

--- codigo.js
+++ codigo.js
@@ -1,59 +1,58 @@
-function mostrar()
-{
-    let sumaDias = 0;
-    let edad = 0;
-    let edadEnDias = 0;
-    let ndia = 0;
-    let nmes = 0;
-    let diferenciaAnoPasado = 0;
-    let fdia = 0;
-    let fmes = 0;
-    let diferenciaAnoActual = 0;
+function mostrar() {
+  let sumaDias = 0;
+  let edad = 0;
+  let edadEnDias = 0;
+  let ndia = 0;
+  let nmes = 0;
+  let diferenciaAnoPasado = 0;
+  let fdia = 0;
+  let fmes = 0;
+  let diferenciaAnoActual = 0;
 
-    //bienvendia
-    alert("Sacrificial Suicide 💀🤙 Castill0.666. Derechos Reservados. @2023.")
+  //bienvendia
+  alert("Sacrificial Suicide 💀🤙 Castill0.666. Derechos Reservados. @2023.");
 
-    //inputs
-    do{
-        ndia = prompt("Ingrese el dia de nacimiento 👼🏾");
-    }while((ndia < 1 || ndia > 31) || (isNaN(ndia)));
+  //inputs
+  do {
+    ndia = prompt("Ingrese el dia de nacimiento 👼🏾");
+  } while (ndia < 1 || ndia > 31 || isNaN(ndia));
 
-    do{
-        nmes = parseInt(prompt("Ingrese el mes de nacimiento 👼🏽"));
-    }while((nmes < 1 || nmes > 12) || (isNaN(nmes)));
-    
-    do{
-        edad = parseInt(prompt("Ingrese su edad 🤬"));
-    }while((edad < 0 || edad > 100) || (isNaN(edad)));
+  do {
+    nmes = parseInt(prompt("Ingrese el mes de nacimiento 👼🏽"));
+  } while (nmes < 1 || nmes > 12 || isNaN(nmes));
 
-    do{
-        bandera = prompt("Los ha cumplido este año? 🥴(s/n)");
-        bandera = bandera.toLowerCase();
-    }while(bandera != "s" && bandera != "n")
+  do {
+    edad = parseInt(prompt("Ingrese su edad 🤬"));
+  } while (edad < 0 || edad > 100 || isNaN(edad));
 
-    do{
-        fmes = parseInt(prompt("Ingrese el mes actual ✡️(1-12)"));
-    }while((fmes < 1 || fmes > 12) || (isNaN(fmes)));
+  do {
+    bandera = prompt("Los ha cumplido este año? 🥴(s/n)");
+    bandera = bandera.toLowerCase();
+  } while (bandera != "s" && bandera != "n");
 
-    do{
-        fdia = parseInt(prompt("Ingrese el dia actual 🐺(01-31)"));
-    }while((fdia < 1 || fdia > 31) || (isNaN(fdia)));
+  do {
+    fmes = parseInt(prompt("Ingrese el mes actual ✡️(1-12)"));
+  } while (fmes < 1 || fmes > 12 || isNaN(fmes));
 
-    //procesamientos
-    if(bandera == "s"){
-        diferenciaAnoActual = (30- ndia) + ((fmes - nmes - 1) * 30) + fdia;
-    }else{
-        diferenciaAnoPasado = (30 - ndia) + ((12 - nmes) * 30);
-        diferenciaAnoActual = fdia + ((fmes - 1) * 30); 
-    }
+  do {
+    fdia = parseInt(prompt("Ingrese el dia actual 🐺(01-31)"));
+  } while (fdia < 1 || fdia > 31 || isNaN(fdia));
 
-    //outputs
-    if(isNaN(ndia) || isNaN(nmes) || isNaN(edad) || isNaN(fmes) || isNaN(fdia)){
-        alert("Ha ingresado datos erroneos 🖕🚫")
-    }else{
-        edadEnDias = edad * 365.256;
-        edadEnDias = Math.ceil(edadEnDias);
-        sumaDias = edadEnDias + diferenciaAnoActual + diferenciaAnoPasado;
-        alert("Usted lleva " + sumaDias + " dias en la tierra, matese 🧨💀🤙");
-    }
+  //procesamientos
+  if (bandera == "s") {
+    diferenciaAnoActual = 30 - ndia + (fmes - nmes - 1) * 30 + fdia;
+  } else {
+    diferenciaAnoPasado = 30 - ndia + (12 - nmes) * 30;
+    diferenciaAnoActual = fdia + (fmes - 1) * 30;
+  }
+
+  //outputs
+  if (isNaN(ndia) || isNaN(nmes) || isNaN(edad) || isNaN(fmes) || isNaN(fdia)) {
+    alert("Ha ingresado datos erroneos 🖕🚫");
+  } else {
+    edadEnDias = edad * 365.256;
+    edadEnDias = Math.ceil(edadEnDias);
+    sumaDias = edadEnDias + diferenciaAnoActual + diferenciaAnoPasado;
+    alert("Usted lleva " + sumaDias + " dias en la tierra, matese 🧨💀🤙");
+  }
 }
